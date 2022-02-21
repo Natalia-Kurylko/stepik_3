87ac2ad36871aff16d9e32cfa8c158f22fe9f1df
Natalia-Kurylko <nataliahajduk27@gmail.com>
2022-02-21 14:19:02 +0200

    my commit

---
+++ test_items.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn")
