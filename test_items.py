f62a3d326e6b381864da78d801625730ade313fa
Natalia Kurylko <55461760+Natalia-Kurylko@users.noreply.github.com>
2022-02-21 19:54:18 +0200

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,4 +6,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_add_butthon(browser):
     browser.get(link)
-    assert browser.find_element(By.CSS_SELECTOR, "button.btn")
+    assert browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
